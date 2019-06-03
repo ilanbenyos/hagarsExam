6087b5fdf76a1241a331e6c7c7b5d7aaaf8b62ea
ilanbenyos <ilan_ben@hotmal.com>
2019-06-03 08:19:43 +0300

    Updates

---
+++ precache-manifest.4b9b27f8dc0c464401e6e62cceffa828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a815039194aa4ba61781723ef17cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6fde6ea976d02273cf8",
+    "url": "/static/css/main.7ff9419a.chunk.css"
+  },
+  {
+    "revision": "0a4aa840e100938454d0",
+    "url": "/static/js/2.e87d973b.chunk.js"
+  },
+  {
+    "revision": "c6fde6ea976d02273cf8",
+    "url": "/static/js/main.14b63951.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
