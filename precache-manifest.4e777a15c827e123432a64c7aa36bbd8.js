46d8217d2e0d68a6865c645aa9e2d9cb729c685a
ilanbenyos <ilan_ben@hotmal.com>
2019-06-03 16:59:06 +0300

    Updates

---
+++ precache-manifest.4e777a15c827e123432a64c7aa36bbd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e3e32c928f92ee7634bbf83a8cef51",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "7a98796a8c5648c7b816",
+    "url": "/public/static/css/main.7ff9419a.chunk.css"
+  },
+  {
+    "revision": "44cb7402706a296b098f",
+    "url": "/public/static/js/2.46397269.chunk.js"
+  },
+  {
+    "revision": "7a98796a8c5648c7b816",
+    "url": "/public/static/js/main.3cdc997c.chunk.js"
+  },
+  {
+    "revision": "71839442e14b9da6d00a",
+    "url": "/public/static/js/runtime~main.de5d37ed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hagarsExam/precache-manifest.86dec3f0872dd3d85365a73366e15dfa.js"
+  "/public/precache-manifest.4e777a15c827e123432a64c7aa36bbd8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hagarsExam/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
