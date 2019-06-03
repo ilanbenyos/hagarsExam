9fe9e722854e22e58a9bf33644b5ab29c6e4aced
ilanbenyos <ilan_ben@hotmal.com>
2019-06-03 08:39:26 +0300

    Updates

---
+++ precache-manifest.b068f1f073f89f707a675753c6dcc614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fbc075ce08c8becc38b754d15d4b51",
+    "url": "/hagarsExam/index.html"
+  },
+  {
+    "revision": "c6fde6ea976d02273cf8",
+    "url": "/hagarsExam/static/css/main.7ff9419a.chunk.css"
+  },
+  {
+    "revision": "0a4aa840e100938454d0",
+    "url": "/hagarsExam/static/js/2.e87d973b.chunk.js"
+  },
+  {
+    "revision": "c6fde6ea976d02273cf8",
+    "url": "/hagarsExam/static/js/main.14b63951.chunk.js"
+  },
+  {
+    "revision": "735ae15d7c65489b6d51",
+    "url": "/hagarsExam/static/js/runtime~main.2a0c331c.js"
+  }
+]);
